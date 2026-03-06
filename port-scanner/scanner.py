ac91cad1defad580e9988a28064c113bc8fddda2
Vivek Singh Bisht <your_email@example.com>
2026-03-06 15:47:55 +0530

    updated

--- port-scanner/scanner.py
+++ port-scanner/scanner.py
@@ -1,17 +1,25 @@
+
 import socket
 
-target = input("Enter target IP: ")
+target = input("Enter target IP or domain: ")
 
-for port in range(1,1025):
+# Convert domain to IP
+target_ip = socket.gethostbyname(target)
+
+print("Scanning:", target_ip)
+
+for port in range(1, 1025):
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(1)
 
-    result = s.connect_ex((target, port))
+    result = s.connect_ex((target_ip, port))
 
     if result == 0:
         print(f"Port {port} is OPEN")
 
     s.close()
 
-
     
+
+
